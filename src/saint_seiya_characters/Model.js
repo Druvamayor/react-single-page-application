d309e2bb5c4231451d7e1ad9f547f2faba285ce1
Juan Mendez Mora <99179311+Druvamayor@users.noreply.github.com>
2023-11-10 20:23:31 -0500

    Update Model.js

--- src/saint_seiya_characters/Model.js
+++ src/saint_seiya_characters/Model.js
@@ -4,7 +4,7 @@ export default function Model() {
   const {characters}=useParams();
   return (
       <div className='m-4'>
-      <img className="img-fluid mb-4 border border-5 rounded-6 border-danger bg-whitesmoke" src={`https://img-12.stickers.cloud/packs/be90c707-3de3-4a0b-853a-24be4f86ea6a/webp/${characters}.webp`} alt={characters} />
+      <img className="img-fluid mb-4 border border-5 rounded-6 border-danger bg-whitesmoke" src={`https://raw.githubusercontent.com/Druvamayor/react-single-page-application/main/src/saint_seiya_characters/assets/${characters}.webp`} alt={characters} />
       <figcaption id="Namecharacter" className="fs-3 fw-bold text-light">{characters}</figcaption>
     </div>
 
